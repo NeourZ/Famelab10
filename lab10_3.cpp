b73e441b65cf607d74e4ed8412e64cfbb42d25fc
Fame <pattanitfame@gmail.com>
2023-12-19 16:59:12 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    int i = 0 ;
+    double sum = 0, sum2 = 0 ;
+    string textline;
+    while(getline(source, textline)){
+        double x = stod(textline);
+        sum = sum + x ;
+        sum2 = sum2 + (x * x) ; 
+        i++ ;
+    }
+    double mean = sum / i ;
+    source.close();
+
+    cout << "Number of data = " << i << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << mean << '\n';
+    cout << "Standard deviation = " << sqrt((sum2/i)-(mean * mean));
+}
